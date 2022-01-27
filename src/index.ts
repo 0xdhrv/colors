4a0d7caa91a7f7bb6f52e7194cb1b7eb53cdc789
Dhruv Suthar <hi@dhrv.pw>
2022-01-27 10:06:08 +0530

    fix: add gray

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './dark';
 export * from './light';
+export { gray } from './gray';
 export { black } from './black';
 export { blackA } from './blackA';
 export { white } from './white';
